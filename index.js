cb00a705c2d4c226283418c0ca189f9392f69efa
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-24 14:01:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Cat {
+    constructor(name){
+        this.name = name 
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+  }
+  
+  class Dog {
+    constructor(name){
+        this.name = name 
+    }
+
+    speak(){
+        return `${this.name} says woof!`
+    }
+  }
+  
+  class Bird {
+    constructor(name){
+        this.name = name 
+    }
+
+    speak(){
+        return `Its me! ${this.name}, says parrot!`
+    }
+  }
